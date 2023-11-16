efda62e61ce95a74202fb922710eababeba8bec9
Rodolfo <fororus12@gmail.com>
2023-11-16 10:59:10 -0400

    Primera version sin BDD

---
+++ server/sockets/socket.js
@@ -0,0 +1,67 @@
+const { io } = require('../server');
+
+const Usuarios = require('../clases/usuarios');
+const crearMensaje = require('../utils/utils');
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+
+    client.on('entrarChat', ( data, callback )=>{
+        if(!data.nombre || !data.sala){
+            return callback({
+                error: true,
+                mensaje: 'El nombre es necesario',
+            });
+        };
+
+        client.join(data.sala);
+
+        const personas = usuarios.agregarPersona( client.id, data.nombre, data.sala );
+
+        client.broadcast.to( data.sala ).emit('listaPersona', usuarios.getPersonasPorSala( data.sala ) );
+
+        callback( usuarios.getPersonasPorSala( data.sala) );
+    });
+
+    client.on('disconnect', () => {
+
+        const personaBorrada = usuarios.eliminarPersona( client.id );
+
+        client.broadcast.to( personaBorrada.sala ).emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } abandono el chat`));
+        client.broadcast.to( personaBorrada.sala ).emit('listaPersona', usuarios.getPersonasPorSala( personaBorrada.sala ) );
+
+    });
+
+    client.on('crearMensaje', (data) => {
+
+        const persona = usuarios.getPersona( client.id );
+
+        const mensaje = crearMensaje( persona.nombre, data.mensaje );
+
+        client.broadcast.to( persona.sala ).emit('crearMensaje', mensaje )
+
+    });
+
+    client.on('mensajePrivado', (data) => {
+
+        const { /* id, */ mensaje, para } = data;
+
+        // if (!id) {
+        //     return new Error(`No hay id`);
+        // }
+
+        if (!mensaje) {
+            return new Error(`No hay mensaje`);
+        }
+        
+        const persona = usuarios.getPersona( client.id );
+        client.broadcast.to( para ).emit('mensajePrivado', crearMensaje( persona.nombre , data.mensaje))
+
+    })
+
+    
+    
+
+});
